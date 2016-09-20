dfd50578c803f0ffffa08e0f1b01d8ac36e8d8b0
Sanchit <dev@minutephp.com>
2016-09-20 06:24:57 +0530

    updated to version v0.0.1

---
+++ js/src/angular-markdown.js
@@ -0,0 +1,31 @@
+/// <reference path="../../../minute/_all.d.ts" />
+var Directives;
+(function (Directives) {
+    var AngularMarkdownService = (function () {
+        function AngularMarkdownService() {
+            var _this = this;
+            this.converter = new window['showdown'].Converter();
+            this.convert = function (text) {
+                //var txt = (text || '').replace(/\r?\n/g, '<br/>');
+                var html = _this.converter.makeHtml(text);
+                html = html.replace(/^<p>|<\/p>$/g, '');
+                html = html.replace(/<soft-br\s*\/?>/g, ' <br class="hidden-xs hidden-sm" />');
+                html = html.replace(/<youtube>(?:.*?v=)?(.*?)<\/youtube>/g, '<div class="thumbnail"><div class="embed-responsive embed-responsive-4by3"><iframe width="560" height="315" src="https://www.youtube.com/embed/$1?rel=0&amp;controls=0&amp;hd=1&amp;showinfo=0" frameborder="0" allowfullscreen></iframe></div></div>');
+                return html;
+            };
+        }
+        return AngularMarkdownService;
+    }());
+    Directives.AngularMarkdownService = AngularMarkdownService;
+    function markDownFilter($sce) {
+        return function (text) {
+            var markdown = new AngularMarkdownService();
+            var html = markdown.convert(text || '');
+            return $sce.trustAsHtml(html);
+        };
+    }
+    Directives.markDownFilter = markDownFilter;
+    angular.module('AngularMarkdown', [])
+        .service("$markdown", AngularMarkdownService)
+        .filter('markdown', ['$sce', markDownFilter]);
+})(Directives || (Directives = {}));
---
+++ js/src/angular-markdown.ts
@@ -0,0 +1,31 @@
+/// <reference path="../../../minute/_all.d.ts" />
+
+module Directives {
+    export class AngularMarkdownService {
+        public converter = new window['showdown'].Converter();
+
+        convert = (text:string):string => {
+            //var txt = (text || '').replace(/\r?\n/g, '<br/>');
+            var html = this.converter.makeHtml(text);
+
+            html = html.replace(/^<p>|<\/p>$/g, '');
+            html = html.replace(/<soft-br\s*\/?>/g, ' <br class="hidden-xs hidden-sm" />');
+            html = html.replace(/<youtube>(?:.*?v=)?(.*?)<\/youtube>/g, '<div class="thumbnail"><div class="embed-responsive embed-responsive-4by3"><iframe width="560" height="315" src="https://www.youtube.com/embed/$1?rel=0&amp;controls=0&amp;hd=1&amp;showinfo=0" frameborder="0" allowfullscreen></iframe></div></div>');
+
+            return html;
+        };
+    }
+
+    export function markDownFilter($sce) {
+        return function (text) {
+            let markdown = new AngularMarkdownService();
+            let html = markdown.convert(text || '');
+
+            return $sce.trustAsHtml(html);
+        };
+    }
+
+    angular.module('AngularMarkdown', [])
+        .service("$markdown", AngularMarkdownService)
+        .filter('markdown', ['$sce', markDownFilter]);
+}
